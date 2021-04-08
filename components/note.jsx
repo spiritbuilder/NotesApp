5fa8aed3b738f7740a6d21e239afa85511e9d3b6
spiritbuilder <olaifatimothy@gmail.com>
2021-04-08 14:52:04 +0100

    first commit

---
+++ components/note.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const note = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default note
---
+++ components/notes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const notes = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default notes
